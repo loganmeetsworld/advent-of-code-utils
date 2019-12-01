745da33b66d102159d6f08de95099ae115c93669
Logan McDonald <loganmcdona11@gmail.com>
2019-12-01 10:09:15 -0500

    notes

---
+++ aoc.py
@@ -0,0 +1,6 @@
+# ability to fetch and give input
+# ability to fetch and save input
+# ability to fetch and save problem content
+# ability to run tests
+# ability to submit only after the tests pass
+# ability to submit answers to parts a, b
